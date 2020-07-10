0007318558cbdf0940918feeb87965a1cc9e9daa
SantiagoAlvarez95 <santiagoalv1995@gmail.com>
2020-07-10 17:43:38 -0300

    practica

---
+++ practica/src/main/java/NewClass.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class NewClass {
+
+    public static void main(String [] args) {
+        
+       sasasa s=new sasasa();
+       s.setVisible(true);
+        
+    }
+    
+    
+}
